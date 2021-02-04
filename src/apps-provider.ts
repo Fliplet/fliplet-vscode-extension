90eaa2a3b57fa5e21900f0563e9417892fdaeaa9
Nicholas Valbusa <squallstar@gmail.com>
2021-02-04 23:17:07 +0100

    refactor to typescript

---
+++ src/apps-provider.ts
@@ -0,0 +1,42 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import * as moment from 'moment';
+
+export class AppsProvider implements vscode.TreeDataProvider<App> {
+  constructor(private state: any, private api: object) {}
+
+  private _onDidChangeTreeData: vscode.EventEmitter<App | undefined | null | void> = new vscode.EventEmitter<App | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<App | undefined | null | void> = this._onDidChangeTreeData.event;
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: App): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: App): Thenable<App[]> {
+    return Promise.resolve(this.state.apps.map((app: any) => {
+      return new App(app, 1);
+    }));
+  }
+}
+
+class App extends vscode.TreeItem {
+  constructor(
+    public readonly app: any,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState
+  ) {
+    super(app.label, collapsibleState);
+    this.tooltip = `Last update ${moment(app.updatedAt).fromNow()}`;
+    this.description = app.name;
+  }
+
+  iconPath = {
+    light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+    dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+  };
+}
---
+++ src/extension.ts
@@ -0,0 +1,167 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+const axios = require("axios").default;
+const _ = require("lodash");
+
+import { env } from "vscode";
+import * as keytarType from "keytar";
+
+declare const __webpack_require__: typeof require;
+declare const __non_webpack_require__: typeof require;
+
+function getNodeModule<T>(moduleName: string): T | undefined {
+  const r =
+    typeof __webpack_require__ === "function"
+      ? __non_webpack_require__
+      : require;
+  try {
+    return r(`${env.appRoot}/node_modules.asar/${moduleName}`);
+  } catch (err) {
+    // Not in ASAR.
+  }
+  try {
+    return r(`${env.appRoot}/node_modules/${moduleName}`);
+  } catch (err) {
+    // Not available.
+  }
+  return undefined;
+}
+
+const keytar = getNodeModule<typeof keytarType>("keytar");
+import { AppsProvider } from "./apps-provider";
+
+const extensionId = "fliplet.vscode";
+
+let API: any;
+let statusBarItem: vscode.StatusBarItem;
+
+const state = {
+  apps: [],
+};
+
+const tree = new AppsProvider(state, API);
+
+const baseURL = "https://api.fliplet.test/";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export async function activate(context: vscode.ExtensionContext) {
+  // Register a URI handler for the authentication callback
+  vscode.window.registerUriHandler({
+    handleUri: function (uri) {
+      // Add your code for what to do when the authentication completes here.
+      if (uri.path.indexOf("/auth-complete") === 0) {
+        const authToken = uri.query.replace("auth_token=", "") as string;
+
+        return verify(authToken);
+      }
+    },
+  });
+
+  let disposable = vscode.commands.registerCommand(
+    "fliplet.signin",
+    async function () {
+      const callbackUri = await vscode.env.asExternalUri(
+        vscode.Uri.parse(
+          `${vscode.env.uriScheme}://${extensionId}/auth-complete`
+        )
+      );
+
+      const uri = vscode.Uri.parse(
+        `${baseURL}v1/auth/third-party?redirect=${encodeURIComponent(
+          callbackUri.toString()
+        )}&responseType=code&source=VSCode&title=Sign%20in%20to%20Authorize%20Visual%20Studio%20Code`
+      );
+
+      vscode.env.openExternal(uri);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+
+  if (keytar) {
+    const authToken: string | null = await keytar.getPassword(
+      extensionId,
+      "authToken"
+    );
+
+    if (authToken) {
+      verify(authToken);
+    }
+  }
+}
+
+vscode.window.registerTreeDataProvider("apps", tree);
+vscode.commands.registerCommand("apps.refresh", function () {
+  fetchApps();
+});
+
+vscode.commands.registerCommand("apps.logout", async function () {
+  await API.post("v1/auth/logout");
+
+  if (keytar) {
+    keytar.deletePassword(extensionId, "authToken");
+  }
+
+  vscode.commands.executeCommand("setContext", "loggedIn", false);
+
+  if (statusBarItem) {
+    statusBarItem.hide();
+  }
+});
+
+async function fetchApps() {
+  const apps = (await API.get("v1/apps")).data.apps;
+
+  state.apps = apps;
+  tree.refresh();
+}
+
+async function verify(authToken: string) {
+  /*return vscode.window.withProgress({
+    location: vscode.ProgressLocation.Window,
+    cancellable: false,
+    title: 'Fliplet'
+	}).then(async (progress) => {
+  */
+
+  API = axios.create({
+    baseURL: "https://api.fliplet.test/",
+    headers: { "Auth-token": authToken, "X-Third-Party": extensionId },
+  });
+
+  vscode.commands.executeCommand("setContext", "loggedIn", true);
+
+  try {
+    const user = (await API.get("v1/user")).data.user;
+    const organization = _.first(
+      (await API.get("v1/organizations")).data.organizations
+    );
+
+    if (keytar) {
+      await keytar.setPassword(extensionId, "authToken", authToken);
+    }
+
+    if (!statusBarItem) {
+      statusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+        1000
+      );
+    }
+
+    statusBarItem.text = `${user.fullName} (${organization.name})`;
+    //statusBarItem.command = 'extension.sayHello';
+    statusBarItem.show();
+
+    fetchApps();
+  } catch (err) {
+    vscode.commands.executeCommand("setContext", "loggedIn", false);
+    console.error(err);
+  }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
